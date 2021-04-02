7c3582320fdd59203cae327d00ddd1c70d4cd651
张锐 <zhangrui01@shouqianba.com>
2021-04-02 18:19:43 +0800

    feat: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    // sourceType: "module",
+  },
+  env: {
+    browser: true,  //支持浏览器的全局变量
+  },
+  rules: {
+    // indent: ["error", 2],
+    // quotes: ["error", "double"],
+    // semi: ["error", "always"],
+    // "no-console": "error",
+    // "arrow-parens": 0,
+  },
+  // extends: "airbnb", //继承airbnb的eslint规则
+};
+
+//.eslintrc文件配置 https://www.jianshu.com/p/a4966ddf9b0c
